7659c1786c64d098eae5b7f0ae464b885f00dfc8
Lmar-O <115730198+Lmar-O@users.noreply.github.com>
2025-07-29 14:46:15 -0500

    Fix wheat image display: Use single image instead of three duplicates
    - wheat.png already contains 3 wheat stalks
    - Replace three separate img tags with one
    - Update className to wheat-image for better styling
    - Update alt text to reflect multiple stalks

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -28,9 +28,7 @@ const LandingPage = () => {
             
             <div className="hero-graphic">
               <div className="wheat-stalks">
-                <img src="/images/wheat.png" alt="Wheat stalk" className="wheat-stalk wheat-1" />
-                <img src="/images/wheat.png" alt="Wheat stalk" className="wheat-stalk wheat-2" />
-                <img src="/images/wheat.png" alt="Wheat stalk" className="wheat-stalk wheat-3" />
+                <img src="/images/wheat.png" alt="Wheat stalks" className="wheat-image" />
               </div>
             </div>
           </div>
